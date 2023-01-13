297c88edb9b3b8327929727f56c8b256ab4579ca
Haishan <haishanhan@gmail.com>
2023-01-13 20:47:22 +0800

    Switch to use vitePreprocess

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,10 @@
 import adapter from '@sveltejs/adapter-node';
-import preprocess from 'svelte-preprocess';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: preprocess(),
+  // https://kit.svelte.dev/docs/integrations#preprocessors
+  preprocess: vitePreprocess(),
 
   kit: {
     adapter: adapter(),
