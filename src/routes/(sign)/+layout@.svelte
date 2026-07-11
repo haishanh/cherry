791537af2607e618ce3ce47df38307384ff6b7e7
haishan <haishanhan@gmail.com>
2026-07-11 13:22:15 +0800

    Convert sign layout styles to CSS

--- src/routes/(sign)/+layout@.svelte
+++ src/routes/(sign)/+layout@.svelte
@@ -8,10 +8,11 @@
   </main>
 </div>
 
-<style lang="scss">
+<style>
   .layout {
     padding: 20px;
   }
+
   main {
     max-width: 600px;
     margin: 0 auto;
@@ -19,13 +20,16 @@
     background-color: var(--bg-card);
     border-radius: 15px;
     --shadow-color: 0deg 0% 53%;
-    // prettier-ignore
+    /* prettier-ignore */
     --shadow-elevation-medium:
       0px 0.2px 0.2px hsl(var(--shadow-color) / 0.36),
       0px 0.7px 0.8px -0.8px hsl(var(--shadow-color) / 0.36),
       0px 1.7px 1.9px -1.7px hsl(var(--shadow-color) / 0.36),
       -0.1px 4.1px 4.6px -2.5px hsl(var(--shadow-color) / 0.36);
-    @media (prefers-color-scheme: light) {
+  }
+
+  @media (prefers-color-scheme: light) {
+    main {
       box-shadow: var(--shadow-elevation-medium);
     }
   }
